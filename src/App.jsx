624c5b86e5757f694c4288b0d733e5ade1b2dcf8
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-28 20:13:06 +0000

    Added a simulated incoming messages.

--- src/App.jsx
+++ src/App.jsx
@@ -31,5 +31,18 @@ class App extends Component {
     </section>
     )
   }
+  // after mounting
+  componentDidMount() {
+    console.log('componentDidMount <App />');
+    setTimeout(() => {
+      console.log('Simulating incoming message');
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+      const messages = this.state.messages.concat(newMessage); // concat used because we can't directly change the state once it's been set.
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages});
+    }, 3000);
+  }
 }
 export default App;
