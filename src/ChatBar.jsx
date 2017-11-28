894406dfe44f9bd385efd35e34e72bf79a561935
Quinlan Jager <quinlan.jager@gmail.com>
2017-11-28 22:58:11 +0000

    Changes username on blur.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -6,6 +6,13 @@ class ChatBar extends Component {
     this.handleMessageSending = this.handleMessageSending.bind(this);
   }
 
+  handleEnterPress(event){
+    if(event.key === 'Enter'){
+      event.target.blur();
+      event.target.nextSibling.focus();
+    }
+  }
+
   handleMessageSending(event){
     if(event.key === 'Enter'){
       if(!event.target.value){
@@ -19,7 +26,7 @@ class ChatBar extends Component {
   render () {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" onChange={ this.props.changeUsername } defaultValue={ this.props.currentUser } />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" onKeyPress={ this.handleEnterPress } onBlur={ this.props.changeUsername } defaultValue={ this.props.currentUser } />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onChange={ this.handleChangeContent } onKeyPress={ this.handleMessageSending }/>
       </footer>
     );
